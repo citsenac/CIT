1ce8743c6869f2a283f7944848e8135632700566
bruno <brunovicente.dev@gmail.com>
2024-06-06 15:55:10 -0300

    Added block scroll

--- CIT/src/components/Navbar/Navbar.tsx
+++ CIT/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,36 @@
 import { useState } from "react";
-
+import scrollLock from 'scroll-lock';
 export default function Navbar() {
     const [open, setOpen] = useState(false);
+
+    const sections: {name: string, target: string}[] = [
+            {
+                name: "Sobre nós",
+                target: "sobre-nos"
+            },
+            {
+                name: "Projetos",
+                target: "projetos"
+            },
+            {
+                name: "Depoimentos",
+                target: "depoimentos"
+            },
+            {
+                name: "Equipe",
+                target: "equipe"
+            },
+            {
+                name: "Contato",
+                target: "contato"
+            }
+        ];
+
+    if (open) {
+        scrollLock.disablePageScroll();
+    } else {
+        scrollLock.enablePageScroll();
+    }
 
     const openMenu = () => {
         setOpen( prev => {
@@ -11,25 +40,35 @@ export default function Navbar() {
     }
 
     return (
-        <div className="relative w-full p-6">
+        <div className="w-full p-6">
             <nav className="hidden md:flex text-red-500 sf-pro-display mx-auto max-w-3xl">
                 Sobre nós
             </nav>
 
-            <button onClick={openMenu}>
+            <button className="md:hidden" onClick={openMenu}>
                 <img src="/icons/hamb-nav.svg" alt="Ícone do menu" className="w-8 h-8" />
             </button>
 
             { open && (
-                <nav className="absolute top-0 left-0 w-2/3 p-6 h-screen bg-gradient-to-b from-[#232323] to-[#262626]">
-                    <button onClick={openMenu} className="mb-10">
-                        <img src="/icons/hamb-nav.svg" alt="Ícone do menu" className="w-8 h-8" />
-                    </button>
+                <>
+                    <div className="h-screen w-screen absolute top-0 left-0 bg-black/40 z-40"></div>
 
-                    <ul className="sf-pro-display text-sm text-white">
-                        <li>Sobre Nós</li>
-                    </ul>
-                </nav>
+                    <nav className="absolute z-50 top-0 left-0 w-2/3 p-6 h-screen bg-gradient-to-b from-[#232323] to-[#262626]">
+                        <button onClick={openMenu} className="mb-10">
+                            <img src="/icons/hamb-nav.svg" alt="Ícone do menu" className="w-8 h-8" />
+                        </button>
+
+                        <ul className="sf-pro-display text-sm text-white">
+                            {sections.map((section: {name: string, target: string}) => {
+                                return (
+                                    <li className="py-6 first:border-t border-b border-white" key={section.name}>
+                                        <a className="block w-full" href={`#${section.target}`}>{section.name}</a>
+                                    </li>
+                                )
+                        })}
+                        </ul>
+                    </nav>
+                </>
             )}
         </div>
     )
