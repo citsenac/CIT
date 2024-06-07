29abf61bccf3135b2bd80a3ac68e1f10e4c916fb
bruno <brunovicente.dev@gmail.com>
2024-06-07 16:54:23 -0300

    Added overflow x hidden

--- CIT/src/App.tsx
+++ CIT/src/App.tsx
@@ -5,7 +5,7 @@ import SobreNos from "./components/SobreNos/SobreNos"
 function App() {
 
   return (
-    <main className="bg-gradient-to-b from-[#232323] via-[#262626] via-63% to-[#161017] min-h-screen">
+    <main className="bg-gradient-to-b from-[#232323] via-[#262626] via-63% to-[#161017] min-h-screen overflow-x-hidden">
       <Navbar />
       <Header />
       <SobreNos />
