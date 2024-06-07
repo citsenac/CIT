7c51ff955fe9e0529bfe54648dba162cf65776c6
bruno <brunovicente.dev@gmail.com>
2024-06-07 16:54:35 -0300

    Added gap between title and text

--- CIT/src/components/SobreNos/Topico.tsx
+++ CIT/src/components/SobreNos/Topico.tsx
@@ -1,6 +1,6 @@
 export default function Topico({children, title, icon}: {children: React.ReactNode, title: string, icon: string}) {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-y-5">
             <div className="flex items-center justify-start gap-x-4">
                 <img src={icon} alt={title} className="w-6 h-6"/>
                 <h3 className="text-white text-2xl font-semibold poppins">
