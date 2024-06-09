ebfd9ad423b04dbee1ac2a87254627a066aad292
bruno <brunovicente.dev@gmail.com>
2024-06-08 23:37:17 -0300

    Added projeto variable

--- CIT/src/components/Projetos/Projeto.tsx
+++ CIT/src/components/Projetos/Projeto.tsx
@@ -1,7 +1,7 @@
-export default function Projeto() {
+export default function Projeto({projeto}: {projeto: {id: number, imgs: string[], title: string, description: string}}) {
     return (
         <div className="h-36 w-56 bg-red-500 rounded-2xl">
-            <img src="test-img.png" alt="Teste" className="h-full w-full object-cover" />
+            <img src="test-img.png" alt={projeto.title} className="h-full w-full object-cover" />
         </div>
     )
 }
