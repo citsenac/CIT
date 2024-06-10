aaaa91d737711e14cba9a98de29064435a7c2a41
bruno <brunovicente.dev@gmail.com>
2024-06-10 14:48:03 -0300

    Changed back to div

--- CIT/src/components/Projetos/Projeto.tsx
+++ CIT/src/components/Projetos/Projeto.tsx
@@ -1,7 +1,7 @@
 export default function Projeto({projeto, toggleModal}: {projeto: {id: string, imgs: string[], title: string, description: string}, toggleModal: (projeto: {id: string, imgs: string[], title: string, description: string}) => void}) {
     return (
-        <a href="#projetos" onClick={() => toggleModal(projeto)} className="h-36 w-56 bg-red-500 rounded-2xl">
+        <div onClick={() => toggleModal(projeto)} className="h-36 w-56 bg-red-500 rounded-2xl">
             <img src="test-img.png" alt={projeto.title} className="h-full w-full object-cover" />
-        </a>
+        </div>
     )
 }
