e0743f46c6fc719c69c7e0da4dc6683037ff634f
bruno <brunovicente.dev@gmail.com>
2024-06-12 11:23:33 -0300

    Added Equipe disciplinar to Equipe

---
+++ CIT/src/components/Equipe/Disciplinar.tsx
@@ -0,0 +1,11 @@
+export default function Disciplinar({funcionario} : {funcionario: {id: string, img: string, nome: string}}) {
+    return (
+        <div className="flex flex-col items-center w-full h-80 relative">
+            <img src={funcionario.img} alt="Imagem do funcionario" className="w-full h-full object-cover rounded-xl" />
+            <div className="absolute w-full py-3 px-6 bottom-0 rounded-b-xl text-white flex justify-between">
+                <div className="w-full h-full bg-[#454545]/60 backdrop-blur-md absolute left-0 top-0 rounded-b-xl"></div>
+                <span className="text-white relative z-1 text-sm poppins font-medium">{funcionario.nome}</span>
+            </div>
+        </div>
+    )
+}
--- CIT/src/components/Equipe/Equipe.tsx
+++ CIT/src/components/Equipe/Equipe.tsx
@@ -1,4 +1,5 @@
 import Aluno from "./Aluno";
+import Disciplinar from "./Disciplinar";
 
 export default function Equipe({target}: {target: string}) {
 
@@ -8,10 +9,17 @@ export default function Equipe({target}: {target: string}) {
         {id: "2",img: "test-profile.png", nome: "Nome do Aluno", linkedin: "https://www.linkedin.com/in/brunovicenters/"}
     ]
 
+    const funcionario = [
+        {id: "0", img: "test-profile.png", nome: "Nome do Funcionario"},
+        {id: "1", img: "test-profile.png", nome: "Nome do Funcionario"},
+        {id: "2",img: "test-profile.png", nome: "Nome do Funcionario"}
+    ]
+
     return (
         <section id={target} className="relative mt-16 mx-2 border-y border-[#606060] flex flex-col items-center">
 
-            <div className="my-16 flex flex-col items-center">
+            <div className="my-16 flex flex-col items-center gap-y-16">
+                {/* Alunos */}
                 <div className="flex flex-col items-center">
                     <h3 className="text-2xl mb-8 font-semibold text-white poppins">
                         Alunos
@@ -20,6 +28,20 @@ export default function Equipe({target}: {target: string}) {
                         {alunos.map((aluno) => (
                             <swiper-slide key={aluno.id}>
                                 <Aluno aluno={aluno} />
+                            </swiper-slide>
+                        ))}
+                    </swiper-container>
+                </div>
+
+                {/* Equipe Disciplinar */}
+                <div className="flex flex-col items-center">
+                    <h3 className="text-2xl mb-8 font-semibold text-white poppins">
+                        Equipe Disciplinar
+                    </h3>
+                    <swiper-container effect="cards" class="w-64" loop='true' grab-cursor="true">
+                        {funcionario.map((funcionario) => (
+                            <swiper-slide key={funcionario.id}>
+                                <Disciplinar funcionario={funcionario} />
                             </swiper-slide>
                         ))}
                     </swiper-container>
