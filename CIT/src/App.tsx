b66b806e9ae07fcf1acf7c72810809b30770d4b1
bruno <brunovicente.dev@gmail.com>
2024-06-12 09:34:34 -0300

    Added integration with firebase authenticated

--- CIT/src/App.tsx
+++ CIT/src/App.tsx
@@ -1,4 +1,4 @@
-// import { onValue, ref } from "firebase/database";
+// import { useEffect, useState } from "react";
 import { register } from 'swiper/element/bundle';
 import Conhecimentos from "./components/Conhecimentos/Conhecimentos";
 import Depoimentos from './components/Depoimentos/Depoimentos';
@@ -7,7 +7,7 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Projetos from "./components/Projetos/Projetos";
 import SobreNos from "./components/SobreNos/SobreNos";
-// import { db } from "./firebase";
+// import { auth, fetchCITData, signInWithEmailAndPassword } from "./firebase";
 
 function App() {
 
@@ -40,23 +40,48 @@ function App() {
     }
   ];
 
-  // const [citData, setCitData] = useState({});
+  // const [citData, setCITData] = useState(null);
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(null);
+
+  // const email = import.meta.env.VITE_FIREBASE_AUTH_EMAIL;
+  // const password = import.meta.env.VITE_FIREBASE_AUTH_PASSWORD;
 
   // useEffect(() => {
-  //   const dbCIT = ref(db);
-  //   onValue(dbCIT, snapshot => {
-  //     const data = snapshot.val()
+  //   const authenticateAndFetchData = async () => {
+  //     try {
+  //       // Authenticate cit
+  //       const userCredential = await signInWithEmailAndPassword(auth, email, password);
+  //       const user = userCredential.user;
+  //       console.log("Authenticated successfully");
 
-  //     if(snapshot.exists()) {
-  //       setCitData(data);
+  //       // Fetch cit data from the root of the database
+  //       const data = await fetchCITData(user.uid);
+  //       setCITData(data);
+  //     } catch (authError) {
+  //       console.error("Authentication error:", authError);
+  //       setError(authError);
+  //     } finally {
+  //       setLoading(false);
   //     }
-  //   })
+  //   };
+  //   authenticateAndFetchData();
   // }, []);
 
-  // console.log(citData);
+  // if (citData) {
+  //   console.log(citData);
+  // }
+  // if (loading) {
+  //   console.log(loading);
+  // }
+  // if (error) {
+  //   console.log(error);
+  // }
 
   return (
     <main className="bg-gradient-to-b from-[#232323] via-[#262626] via-63% to-[#161017] min-h-screen overflow-x-hidden">
+      {/* {!loading &&
+      <> */}
       <Navbar sections={sections} />
       <Header />
       <SobreNos target={sections[0].target} />
@@ -64,6 +89,7 @@ function App() {
       <Conhecimentos target={sections[2].target} />
       <Depoimentos target={sections[3].target} />
       <Equipe target={sections[4].target} />
+      {/* </>} */}
     </main>
   )
 }
--- CIT/src/firebase.tsx
+++ CIT/src/firebase.tsx
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { get, getDatabase, ref } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,5 +18,25 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
 
-export const db = getDatabase(app)
+function fetchCITData(userId) {
+  return new Promise((resolve, reject) => {
+    const userRef = ref(db, `/`);
+    get(userRef)
+      .then(snapshot => {
+        if (snapshot.exists()) {
+          resolve(snapshot.val());
+        } else {
+          reject(new Error("No data available"));
+        }
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
+
+export { app, auth, db, fetchCITData, signInWithEmailAndPassword };
+
