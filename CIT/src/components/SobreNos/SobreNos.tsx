af1f0536a1d856472fcfaca1d1f9fc612e025a2a
bruno <brunovicente.dev@gmail.com>
2024-06-13 16:35:13 -0300

    Made desktop version

--- CIT/src/components/SobreNos/SobreNos.tsx
+++ CIT/src/components/SobreNos/SobreNos.tsx
@@ -3,16 +3,16 @@ import Topico from "./Topico";
 
 export default function SobreNos({target}: {target: string}) {
     return (
-        <section id={target} className="relative mt-24 max-w-[22rem] mx-auto">
+        <section id={target} className="relative mt-24 max-w-[22rem] md:max-w-6xl mx-auto">
 
             {/* Glow */}
-            <div className="absolute rounded-full top-0 h-full w-full bg-[#5200ff] blur-[400px]"></div>
+            <div className="absolute rounded-full top-0 h-full w-full bg-[#5200ff] blur-[400px] md:blur-[800px]"></div>
 
             <GlassContainer>
-                <div className="w-full py-6 px-4">
-                    <h3 className="text-2xl text-white text-center poppins mb-10">O que fazemos?</h3>
+                <div className="w-full py-6 px-4 md:px-40 md:pt-9 md:pb-20 relative z-1">
+                    <h3 className="text-2xl md:text-5xl lg:text-4xl text-white text-center font-medium poppins mb-10">O que fazemos?</h3>
 
-                    <div className="flex flex-col space-y-16">
+                    <div className="flex flex-col md:flex-row space-y-16 md:space-y-0 md:justify-between">
                         <Topico title="Inovação" icon="icons/inovacao.svg">
                             Trazemos inovação em nosso DNA. Os projetos criados aqui utilizam
                             paradigmas, linguagens, plataformas e frameworks modernos,
--- CIT/src/components/SobreNos/Topico.tsx
+++ CIT/src/components/SobreNos/Topico.tsx
@@ -1,13 +1,13 @@
 export default function Topico({children, title, icon}: {children: React.ReactNode, title: string, icon: string}) {
     return (
-        <div className="flex flex-col gap-y-5">
+        <div className="flex flex-col gap-y-5 md:w-96">
             <div className="flex items-center justify-start gap-x-4">
                 <img src={icon} alt={title} className="w-6 h-6"/>
                 <h3 className="text-white text-2xl font-semibold poppins">
                     {title}
                 </h3>
             </div>
-            <p className="text-white sf-pro-display text-sm">{children}</p>
+            <p className="text-white sf-pro-display text-sm md:text-lg">{children}</p>
         </div>
     )
 }
