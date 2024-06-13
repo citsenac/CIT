d8f50e9d5dcc9b7d9be98030550f81e8a9e9eb09
bruno <brunovicente.dev@gmail.com>
2024-06-13 20:06:56 -0300

    Deleted unused bg

--- CIT/src/components/Projetos/Projeto.tsx
+++ CIT/src/components/Projetos/Projeto.tsx
@@ -1,6 +1,6 @@
 export default function Projeto({projeto, toggleModal}: {projeto: {id: string, imgs: string[], title: string, description: string}, toggleModal: (projeto: {id: string, imgs: string[], title: string, description: string}) => void}) {
     return (
-        <div onClick={() => toggleModal(projeto)} className="h-36 w-56 bg-red-500 rounded-2xl cursor-pointer">
+        <div onClick={() => toggleModal(projeto)} className="h-36 w-56 rounded-2xl cursor-pointer">
             <img src="test-img.png" alt={projeto.title} className="h-full w-full object-cover" />
         </div>
     )
