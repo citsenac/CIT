5ed9c8bd51431e4e093bc3f190bd3fcddd4172ae
bruno <brunovicente.dev@gmail.com>
2024-06-13 17:27:56 -0300

    Fixed layout to mobile version

--- CIT/src/components/Projetos/ModalProjeto.tsx
+++ CIT/src/components/Projetos/ModalProjeto.tsx
@@ -3,14 +3,14 @@ export default function ModalProjeto({projeto}: {projeto: {imgs: string[], title
     
     return (
             
-            <div className="flex flex-col items-center w-full h-full">
+            <div className="text-center w-full h-full">
                 <h3 className="mb-4 max-w-56 mx-auto font-semibold bg-gradient-to-br from-white to-[#C9C9C9] text-transparent bg-clip-text inline-block text-2xl text-center poppins">{projeto.title}</h3>
                 <div className='relative w-full flex justify-center items-center mb-6'>
                     { projeto.imgs.length > 1 &&
                         <swiper-container class="w-full" slides-per-view={1} loop={true} centered-slides={true} pagination={true}>
                             {projeto.imgs.map((img, i) => (
                                 <swiper-slide key={i}>
-                                    <img src={img} alt={projeto.title} className="rounded-2xl w-80 h-48 object-cover" />
+                                    <img src={img} alt={projeto.title} className="rounded-2xl w-80 md:w-full h-48 md:h-80 object-cover" />
                                 </swiper-slide>
                             ))
                         }
