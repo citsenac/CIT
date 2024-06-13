1ab7ab272ebafdb70642d1eea58677008bc17634
bruno <brunovicente.dev@gmail.com>
2024-06-13 11:55:30 -0300

    Added success animation

--- CIT/src/components/Contato/Contato.tsx
+++ CIT/src/components/Contato/Contato.tsx
@@ -128,7 +128,11 @@ export default function Contato({target}: {target: string}) {
                     </button>
                     {successAnimation &&
                         <div className="absolute text-white font-medium text-base poppins w-full h-full growing-bg-success left-0 top-0 rounded-lg flex justify-center items-center">
-                            Mensagem enviada!
+                            <svg className="checkmark w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                                <circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none"/>
+                                <path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+                            </svg>
+                            <span className='sr-only'>Desenho de verificado</span>
                         </div>
                     }
                 </div>
