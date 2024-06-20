f1a67882b032e84f82d9e975e5715c7b753b5ab6
bruno <brunovicente.dev@gmail.com>
2024-06-19 21:25:18 -0300

    Fixed import X

--- src/components/Partials/Modal.tsx
+++ src/components/Partials/Modal.tsx
@@ -19,7 +19,7 @@ export default function Modal({children, toggleModal}: {children: React.ReactNod
                 </div>
                 <div className={`${ !close ? 'modal' : 'close-modal'} relative z-50 h-auto w-[95%] md:w-[80%] lg:w-[50%] bg-gradient-to-b from-[#404042] to-[#303030] rounded-2xl border-solid border-2 border-[#989898] pb-4 pt-6 px-4`}>
                     <button onClick={closeModal} className="absolute top-4 right-4">
-                        <img src="/icons/close-white.svg" alt="Fechar modal" className="w-6 h-6" />
+                        <img src="./icons/close-white.svg" alt="Fechar modal" className="w-6 h-6" />
                     </button>
                     {children}
                 </div>
