76a4d58eb7006ab2dc77ce42f6222864f02bfe6b
bruno <brunovicente.dev@gmail.com>
2024-11-05 15:47:19 -0300

    Added conditional coloring

--- src/components/Conhecimentos/Conhecimentos.tsx
+++ src/components/Conhecimentos/Conhecimentos.tsx
@@ -18,6 +18,10 @@ export default function Conhecimentos({target, conhecimentos}: {target: string, 
         scrollabe = true;
     }
 
+    const termBuscado = "java";
+    const corL = "#6692FF";
+    const corR = "#FF30FF";
+
     return (
         <section id={target} className="relative pt-24">
 
@@ -28,9 +32,11 @@ export default function Conhecimentos({target, conhecimentos}: {target: string, 
             <div className="relative py-3 flex flex-col gap-y-3 max-w-full scroller" data-animated={scrollabe ? "true" : 'false'}>
 
                 {conhecimentosDisplay.map((conhecimentoGroup, index) => (
+                    // bg-gradient-to-br from-[#303030] to-[#494949]
                     <ul className="w-full flex flex-wrap scroller-inner odd:self-end" key={index}>
                         {conhecimentoGroup.map((conhecimento) => (
-                            <li className={`capitalize min-w-fit rounded-lg p-2 flex items-center justify-center gap-x-3 h-10 text-sm font-semibold text-white bg-gradient-to-br from-[#303030] to-[#494949] border-solid border-[.5px] border-[#A5A5A5] poppins`} key={conhecimento.id}>
+                            <li className={`capitalize min-w-fit rounded-lg p-2 flex items-center justify-center gap-x-3 h-10 text-sm font-semibold text-white  border-solid border-[.5px] border-[#A5A5A5] poppins`} key={conhecimento.id}
+                                style={{ background: conhecimento.nome.toLowerCase().includes(termBuscado.toLowerCase()) ? `linear-gradient(to right, ${corL} 0%, ${corR} 100%)` : `linear-gradient(to right, #303030 0%, #494949 100%)`}}>
                                 <img src={conhecimento.icon} alt="Stack icon" className="w-6 h-6" />
                                 {conhecimento.nome}
                             </li>
@@ -38,7 +44,8 @@ export default function Conhecimentos({target, conhecimentos}: {target: string, 
 
                         {/* Duplicate to make scroll effect */}
                         {scrollabe && conhecimentoGroup.map((conhecimento) => (
-                            <li className={`capitalize min-w-fit rounded-lg p-2 flex items-center justify-center gap-x-3 h-10 text-sm font-semibold text-white bg-gradient-to-br from-[#303030] to-[#494949] border-solid border-[.5px] border-[#A5A5A5] poppins`} key={conhecimento.id}>
+                            <li className={`capitalize min-w-fit rounded-lg p-2 flex items-center justify-center gap-x-3 h-10 text-sm font-semibold text-white border-solid border-[.5px] border-[#A5A5A5] poppins`} key={conhecimento.id}
+                            style={{ background: conhecimento.nome.toLowerCase().includes(termBuscado.toLowerCase()) ? `linear-gradient(to right, ${corL} 0%, ${corR} 100%)` : `linear-gradient(to right, #303030 0%, #494949 100%)`}}>
                             <img src={conhecimento.icon} alt="Stack icon" className="w-6 h-6" />
                             {conhecimento.nome}
                         </li>
@@ -46,7 +53,8 @@ export default function Conhecimentos({target, conhecimentos}: {target: string, 
 
                         {/* Triplicate to make scroll effect in larger screens */}
                         {scrollabe && width > 1024 && conhecimentoGroup.map((conhecimento) => (
-                            <li className={`capitalize min-w-fit rounded-lg p-2 flex items-center justify-center gap-x-3 h-10 text-sm font-semibold text-white bg-gradient-to-br from-[#303030] to-[#494949] border-solid border-[.5px] border-[#A5A5A5] poppins`} key={conhecimento.id}>
+                            <li className={`capitalize min-w-fit rounded-lg p-2 flex items-center justify-center gap-x-3 h-10 text-sm font-semibold text-white border-solid border-[.5px] border-[#A5A5A5] poppins`} key={conhecimento.id}
+                            style={{ background: conhecimento.nome.toLowerCase().includes(termBuscado.toLowerCase()) ? `linear-gradient(to right, ${corL} 0%, ${corR} 100%)` : `linear-gradient(to right, #303030 0%, #494949 100%)`}}>
                             <img src={conhecimento.icon} alt="Stack icon" className="w-6 h-6" />
                             {conhecimento.nome}
                         </li>
