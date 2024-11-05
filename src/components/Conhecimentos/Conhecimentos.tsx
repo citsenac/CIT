b88352c8e420c64cec141a11739339ba7e694461
bruno <brunovicente.dev@gmail.com>
2024-11-05 17:14:05 -0300

    Updated Stacks to have colors and use it according to search value

--- src/components/Conhecimentos/Conhecimentos.tsx
+++ src/components/Conhecimentos/Conhecimentos.tsx
@@ -18,9 +18,7 @@ export default function Conhecimentos({target, conhecimentos}: {target: string, 
         scrollabe = true;
     }
 
-    const termBuscado = "java";
-    const corL = "#6692FF";
-    const corR = "#FF30FF";
+    const termBuscado = "Js";
 
     return (
         <section id={target} className="relative pt-24">
@@ -34,31 +32,49 @@ export default function Conhecimentos({target, conhecimentos}: {target: string, 
                 {conhecimentosDisplay.map((conhecimentoGroup, index) => (
                     // bg-gradient-to-br from-[#303030] to-[#494949]
                     <ul className="w-full flex flex-wrap scroller-inner odd:self-end" key={index}>
-                        {conhecimentoGroup.map((conhecimento) => (
-                            <li className={`capitalize min-w-fit rounded-lg p-2 flex items-center justify-center gap-x-3 h-10 text-sm font-semibold text-white  border-solid border-[.5px] border-[#A5A5A5] poppins`} key={conhecimento.id}
-                                style={{ background: conhecimento.nome.toLowerCase().includes(termBuscado.toLowerCase()) ? `linear-gradient(to right, ${corL} 0%, ${corR} 100%)` : `linear-gradient(to right, #303030 0%, #494949 100%)`}}>
-                                <img src={conhecimento.icon} alt="Stack icon" className="w-6 h-6" />
-                                {conhecimento.nome}
-                            </li>
-                        ))}
+                        {conhecimentoGroup.map((conhecimento) => {
+                            
+                            const isMatch = conhecimento.nome.toLowerCase().includes(termBuscado.toLowerCase());
+
+                            return (
+                                <li className={`capitalize min-w-fit rounded-lg p-2 flex items-center justify-center gap-x-3 h-10 text-sm font-semibold text-white  border-solid border-[.5px] border-[#A5A5A5] poppins`} key={conhecimento.id}
+                                    style={{background: isMatch ? `linear-gradient(to right, ${conhecimento.corL} 0%, ${conhecimento.corR} 100%)` : `linear-gradient(to right, #303030 0%, #494949 100%)`, 
+                                            borderColor: isMatch ? "#5200ff" : ""}}>
+                                    <img src={conhecimento.icon} alt="Stack icon" className="w-6 h-6" />
+                                    {conhecimento.nome}
+                                </li>
+                            );
+                        })}
 
                         {/* Duplicate to make scroll effect */}
-                        {scrollabe && conhecimentoGroup.map((conhecimento) => (
-                            <li className={`capitalize min-w-fit rounded-lg p-2 flex items-center justify-center gap-x-3 h-10 text-sm font-semibold text-white border-solid border-[.5px] border-[#A5A5A5] poppins`} key={conhecimento.id}
-                            style={{ background: conhecimento.nome.toLowerCase().includes(termBuscado.toLowerCase()) ? `linear-gradient(to right, ${corL} 0%, ${corR} 100%)` : `linear-gradient(to right, #303030 0%, #494949 100%)`}}>
-                            <img src={conhecimento.icon} alt="Stack icon" className="w-6 h-6" />
-                            {conhecimento.nome}
-                        </li>
-                        ))}
+                        {scrollabe && conhecimentoGroup.map((conhecimento) => {
+
+                            const isMatch = conhecimento.nome.toLowerCase().includes(termBuscado.toLowerCase());
+
+                            return (
+                                <li className={`capitalize min-w-fit rounded-lg p-2 flex items-center justify-center gap-x-3 h-10 text-sm font-semibold text-white border-solid border-[.5px] border-[#A5A5A5] poppins`} key={conhecimento.id}
+                                    style={{background: isMatch ? `linear-gradient(to right, ${conhecimento.corL} 0%, ${conhecimento.corR} 100%)` : `linear-gradient(to right, #303030 0%, #494949 100%)`, 
+                                            borderColor: isMatch ? "#5200ff" : ""}}>
+                                    <img src={conhecimento.icon} alt="Stack icon" className="w-6 h-6" />
+                                    {conhecimento.nome}
+                                </li>
+                            )
+                        })}
 
                         {/* Triplicate to make scroll effect in larger screens */}
-                        {scrollabe && width > 1024 && conhecimentoGroup.map((conhecimento) => (
-                            <li className={`capitalize min-w-fit rounded-lg p-2 flex items-center justify-center gap-x-3 h-10 text-sm font-semibold text-white border-solid border-[.5px] border-[#A5A5A5] poppins`} key={conhecimento.id}
-                            style={{ background: conhecimento.nome.toLowerCase().includes(termBuscado.toLowerCase()) ? `linear-gradient(to right, ${corL} 0%, ${corR} 100%)` : `linear-gradient(to right, #303030 0%, #494949 100%)`}}>
-                            <img src={conhecimento.icon} alt="Stack icon" className="w-6 h-6" />
-                            {conhecimento.nome}
-                        </li>
-                        ))}
+                        {scrollabe && width > 1024 && conhecimentoGroup.map((conhecimento) => {
+
+                            const isMatch = conhecimento.nome.toLowerCase().includes(termBuscado.toLowerCase());
+
+                            return (
+                                <li className={`capitalize min-w-fit rounded-lg p-2 flex items-center justify-center gap-x-3 h-10 text-sm font-semibold text-white border-solid border-[.5px] border-[#A5A5A5] poppins`} key={conhecimento.id}
+                                    style={{background: isMatch ? `linear-gradient(to right, ${conhecimento.corL} 0%, ${conhecimento.corR} 100%)` : `linear-gradient(to right, #303030 0%, #494949 100%)`, 
+                                    borderColor: isMatch ? "#5200ff" : ""}}>
+                                    <img src={conhecimento.icon} alt="Stack icon" className="w-6 h-6" />
+                                    {conhecimento.nome}
+                                </li>
+                            )
+                        })}
                     </ul>
                 ))}
 
--- src/models/Stack.tsx
+++ src/models/Stack.tsx
@@ -2,10 +2,14 @@ export default class Stack {
     id: number;
     nome: string;
     icon: string;
+    corL: string;
+    corR: string;
 
-    constructor(id: number, nome: string, icon: string) {
+    constructor(id: number, nome: string, icon: string, corL: string, corR: string) {
         this.id = id;
         this.nome = nome;
         this.icon = icon;
+        this.corL = corL;
+        this.corR = corR;
     }
 }
